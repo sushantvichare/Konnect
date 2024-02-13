387e9899336f130f48bfcbcc1163170ec8ed6af1
sushantvichare <sushantvichare10@gmail.com>
2024-02-13 23:49:49 +0530

    fake data of users added

--- server/index.js
+++ server/index.js
@@ -14,8 +14,6 @@ import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
-import User from "./models/User.js";
-import Post from "./models/Post.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
